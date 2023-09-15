b94c1755dfdb747d73f2f637bd69fd6a17adc8fe
D L <lazykindit@yandex.ru>
2023-09-15 11:41:55 +0300

    3. Каркас шахматной доски [#792 #257891]. Написал метод free для проверки элементов из массива во входном параметре.

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,8 +2,12 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
+/**
+ * Класс, в котором размещена логика игры.
+ */
 public final class Logic {
     private final Figure[] figures = new Figure[32];
     private int index = 0;
@@ -20,7 +24,21 @@ public final class Logic {
         figures[index] = figures[index].copy(dest);
     }
 
+    /**
+     * Проверяет, что фигуры не занимают элементы из массива steps.
+     *
+     * @param steps массив элементов.
+     * @return true - если фигуры не занимают элементы.
+     * @throws OccupiedCellException если фигуры занимают элементы выбрасывает исключение.
+     */
     private boolean free(Cell[] steps) throws OccupiedCellException {
+        for (Figure figure : figures) {
+            for (Cell step : steps) {
+                if (step.equals(figure.position())) {
+                    throw new OccupiedCellException("This cell is already occupied.");
+                }
+            }
+        }
         return true;
     }
 
