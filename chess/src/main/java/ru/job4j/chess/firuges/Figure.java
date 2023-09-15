2f451cf5959c9a440d7af92319acb5232e364113
D L <lazykindit@yandex.ru>
2023-09-15 11:43:40 +0300

    3. Каркас шахматной доски [#792 #257891]. Добавил описание к классам и методам.

--- chess/src/main/java/ru/job4j/chess/firuges/Figure.java
+++ chess/src/main/java/ru/job4j/chess/firuges/Figure.java
@@ -2,16 +2,40 @@ package ru.job4j.chess.firuges;
 
 import ru.job4j.chess.ImpossibleMoveException;
 
+/**
+ * Описывает поведение фигур.
+ */
 public interface Figure {
+    /**
+     * Для получения позиции фигуры на поле.
+     *
+     * @return позицию на поле.
+     */
     Cell position();
 
+    /**
+     * Возвращает массив с клетками, которые должна пройти фигура при своем движении из текущей позиции до клетки dest
+     *
+     * @param dest конечная точка.
+     * @return массив с клетками из текущей позиции до клетки dest.
+     * @throws ImpossibleMoveException если ход сделать нельзя - ловим исключение.
+     */
     Cell[] way(Cell dest) throws ImpossibleMoveException;
 
+    /**
+     * Для реализации имени картинки фигуры.
+     *
+     * @return имя картинки фигуры.
+     */
     default String icon() {
-        return String.format(
-                "%s.png", getClass().getSimpleName()
-        );
+        return String.format("%s.png", getClass().getSimpleName());
     }
 
+    /**
+     * Для создания одноразового объекта класса с позицией dest
+     *
+     * @param dest где будет располагаться фигура.
+     * @return объект.
+     */
     Figure copy(Cell dest);
 }
